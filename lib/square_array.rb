f284a489a146799466985de3621ec8add47b10cf
Kyle Cole <kylepcole@gmail.com>
2019-10-11 23:34:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   squared = []
   element = 0
   while array[element] do
-    squared << (array[element] * array[element])
+    squared << (array[element]**2)
     element += 1
   end
 end
