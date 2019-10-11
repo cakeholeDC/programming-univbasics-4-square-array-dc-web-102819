48f59fb8ffd92801ac16e6b1184288bdc2169d9b
Kyle Cole <kylepcole@gmail.com>
2019-10-11 23:41:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,8 @@ def square_array(array)
   # your code here
   squared = []
   element = 0
-  while element <= array.length do
-    squared_num = (array[element]**2)
-    p squared << squared_num
+  while element < array.length do
+    squared << array[element] ** 2
     element += 1
   end
 end
